0be6b0bb37ee10e0616c2bcd69c2655ec5121b8c
Urge-Smith <randalljo@gmail.com>
2015-12-23 16:55:50 -0700

    Finished all CRUD features for entry form

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   root "demo#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  match ':controller(/:action(/:id))', :via => :get
+  match ':controller(/:action(/:id))', :via => [:get, :post]
+
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
